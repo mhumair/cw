be1e5a5b54dbbbe273c9051b5542ec116f36d602
Muhammad Humair <muhammadhumair001@gmail.com>
2021-12-23 21:09:41 +0500

    Update nodeupdate.sh

--- nodeupdate.sh
+++ nodeupdate.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-
 cd && echo "export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/home/master/bin/npm'
 export NODE_PATH=':/home/master/bin/npm/lib/node_modules'
 export N_PREFIX=$HOME/.n
 export PATH=$N_PREFIX/bin:$PATH
 alias n='/home/master/bin/npm/lib/node_modules/bin/n'" >> $HOME/.bash_aliases &&
-npm config set prefix "/home/master/bin/npm/lib/node_modules"
+npm config set prefix "/home/master/bin/npm/lib/node_modules" &&
+npm install n@latest -g
